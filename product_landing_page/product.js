b38860a80c7e3b1a4357401255feddd2ae304f55
Ravi Patel <rp001713@gmail.com>
2021-04-10 19:55:19 -0500

    RESPONSIVE_WEB_DESIGN_PROJECTS

---
+++ product_landing_page/product.js
@@ -0,0 +1,17 @@
+// $(".hero").bgswitcher({
+//     images: ["img/dcd8aff5cfd04d1688c1fdc91deb505d.png","img/30724492eb644341b56a3c0081068b54.png"],// Background images
+//     effect:"fade" // fade, blind, clip, slide, drop, hide
+// });
+
+// $(".hero").bgswitcher({
+//     images: ["pic1.jpg","pic2.jpg","pic3.jpg"],// Background images
+//     effect:"fade",// fade, blind, clip, slide, drop, hide
+//     interval: 5000,// Interval of switching
+//     loop:true,// Loop the switching
+//     shuffle:false,// Shuffle the order of an images
+//     duration: 5000,// Effect duration
+//     easing:"swing" // Effect easing
+//   });
+  
+  
+    
